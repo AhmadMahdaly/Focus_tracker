9e3aba4a1d43a385e5a3cbb35c8622e745dc6934
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-28 14:53:35 +0200

    Change text field border color to transparent

--- lib/utils/components/text_field_border.dart
+++ lib/utils/components/text_field_border.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 OutlineInputBorder border() {
   return OutlineInputBorder(
     borderRadius: BorderRadius.circular(8),
-    borderSide: const BorderSide(color: Colors.blue, width: 0.4),
+    borderSide: const BorderSide(color: Colors.transparent, width: 0.4),
   );
 }
