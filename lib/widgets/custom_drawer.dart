11b5135c36d611f9079326a8b57c432b384f6da8
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-28 16:57:05 +0200

    Add CustomDrawer widget for improved navigation

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:focus_tracker/screens/achievements_screen.dart';
+import 'package:focus_tracker/screens/settings_screen.dart';
+import 'package:focus_tracker/screens/stats_screen.dart';
+import 'package:focus_tracker/screens/tasks_screen.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: Navigator.of(context).pop,
+                  icon: Icon(Icons.arrow_forward),
+                ),
+                Text(''),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.task_alt_outlined),
+            title: Text("Tasks"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const TasksScreen()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.analytics_outlined),
+            title: Text("Statistics"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const StatsScreen()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.star_border_rounded),
+            title: Text("Achievements"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AchievementsScreen()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text("Settings"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
