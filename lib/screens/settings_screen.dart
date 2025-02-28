fb5a863f4d022006168de6b6fb8789bc766fd3af
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-28 16:56:32 +0200

    Enhance SettingsScreen title style for improved UI consistency

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -35,7 +35,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Settings")),
+      appBar: AppBar(
+        title: const Text(
+          "Settings",
+          style: TextStyle(fontWeight: FontWeight.w200),
+        ),
+      ),
       body: Column(
         children: [
           ListTile(
