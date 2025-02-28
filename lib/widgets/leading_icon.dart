f96f1be39b882da52f19ff9378b4557d60db279a
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-28 21:06:53 +0200

    Add LeadingIcon widget for back navigation functionality

---
+++ lib/widgets/leading_icon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeadingIcon extends StatelessWidget {
+  const LeadingIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Navigator.pop(context); // إزالة جميع الصفحات
+      },
+      icon: Icon(Icons.arrow_back_ios_new, size: 22),
+    );
+  }
+}
