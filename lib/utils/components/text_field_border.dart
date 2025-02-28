9d836a74f85ac42b26ab887748f761c936bc49bf
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-28 21:07:14 +0200

    Update text field border color for improved visibility

--- lib/utils/components/text_field_border.dart
+++ lib/utils/components/text_field_border.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 OutlineInputBorder border() {
   return OutlineInputBorder(
     borderRadius: BorderRadius.circular(8),
-    borderSide: const BorderSide(color: Colors.transparent, width: 0.4),
+    borderSide: BorderSide(color: Colors.black.withAlpha(25), width: 0.4),
   );
 }
