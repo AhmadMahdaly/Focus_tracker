35d321829e7c574374ebd3f87175ecf3fed16c34
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-03-03 01:43:10 +0200

    Refactor NavyBottomBar to simplify navigation and replace GoogleNavBar with custom buttons

--- lib/screens/navy_bottom_bar.dart
+++ lib/screens/navy_bottom_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:focus_tracker/screens/focus_timer_screen.dart';
 import 'package:focus_tracker/screens/tasks_screen.dart';
 import 'package:focus_tracker/widgets/add_task.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:focus_tracker/widgets/custom_drawer.dart';
 
 class NavyBottomBar extends StatefulWidget {
   const NavyBottomBar({super.key});
@@ -12,68 +12,67 @@ class NavyBottomBar extends StatefulWidget {
 }
 
 class _NavyBottomBarState extends State<NavyBottomBar> {
-  int _currentIndex = 0;
-  void _onItemTapped(index) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (mounted) {
-        setState(() => _currentIndex = index);
-      }
-    });
-  }
-
-  List<Widget> pages = [TimerScreen(), TasksScreen(), TimerScreen()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages.elementAt(_currentIndex),
+      body: TimerScreen(),
       bottomNavigationBar: Row(
         children: [
           Expanded(
             child: Container(
               clipBehavior: Clip.antiAlias,
               margin: const EdgeInsets.all(16),
+
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha(25),
-                    blurRadius: 10,
-                    spreadRadius: 5, // changes position of shadow
-                  ),
-                ],
+
+                // color: Colors.white,
               ),
-              child: GNav(
-                onTabChange: _onItemTapped,
-                selectedIndex: _currentIndex,
-                rippleColor:
-                    Colors.white, // tab button ripple color when pressed
-                hoverColor: Colors.white54, // tab button hover color
-                haptic: true, // haptic feedback
-                // tabBorder: Border.all(
-                //   color: Colors.black.withAlpha(25),
-                //   width: 0.5,
-                // ), // tab button border
-                tabBorderRadius: 56,
-                tabActiveBorder: Border.all(
-                  color: Colors.black.withAlpha(25),
-                  width: 2,
-                ), // tab button border
-
-                duration: Duration(milliseconds: 900), // tab animation duration
-                gap: 8, // the tab button gap between icon and text
-                // color: Colors.red[800], // unselected icon color
-                activeColor: Colors.blue, // selected icon and text color
-                iconSize: 24, // tab button icon size
-
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 16,
-                ), // navigation bar padding
-                tabs: [
-                  GButton(icon: Icons.home_filled, text: 'Home'),
-                  GButton(icon: Icons.task_alt_rounded, text: 'Tasks'),
-                  GButton(icon: Icons.access_time_rounded, text: 'Timer'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 56,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(56),
+                        border: Border.all(
+                          // color: Colors.black.withAlpha(25),
+                          width: 1,
+                        ),
+                      ),
+                      child: Row(
+                        spacing: 8,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.home_filled), Text('Home')],
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.task_alt_rounded),
+                    onPressed:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TasksScreen(),
+                          ),
+                        ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.analytics_outlined),
+                    onPressed:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CustomDrawer(),
+                          ),
+                        ),
+                  ),
+                  SizedBox(width: 0),
                 ],
               ),
             ),
