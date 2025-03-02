3809d6f170b8428e8312c1f62fcbb18c454324a9
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-03-03 01:43:44 +0200

    Refactor CustomDrawer to use Scaffold and LeadingIcon for improved UI structure

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:focus_tracker/screens/achievements_screen.dart';
 import 'package:focus_tracker/screens/settings_screen.dart';
-import 'package:focus_tracker/screens/statistics_screen.dart';
 import 'package:focus_tracker/screens/stats_screen.dart';
+import 'package:focus_tracker/widgets/leading_icon.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
+    return Scaffold(
+      appBar: AppBar(leading: LeadingIcon()),
+      body: ListView(
         children: [
-          DrawerHeader(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: Navigator.of(context).pop,
-                  icon: Icon(Icons.arrow_forward),
-                ),
-                Text(''),
-              ],
-            ),
-          ),
-
           ListTile(
             leading: Icon(Icons.analytics_outlined),
             title: Text("Statistics"),
@@ -35,16 +23,7 @@ class CustomDrawer extends StatelessWidget {
               );
             },
           ),
-          ListTile(
-            leading: Icon(Icons.stay_current_landscape),
-            title: Text("Statistics"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => StatisticsScreen()),
-              );
-            },
-          ),
+
           ListTile(
             leading: Icon(Icons.star_border_rounded),
             title: Text("Achievements"),
