ae0325e96c27e641813c29651d273d1a3cae08bc
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-03-03 02:39:16 +0200

    Refactor AchievementsScreen to use Bloc for state management and remove Hive dependency

--- lib/screens/achievements_screen.dart
+++ lib/screens/achievements_screen.dart
@@ -1,82 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:focus_tracker/cubit/stats_cubit/stats_cubit.dart';
 import 'package:focus_tracker/models/achievement_model/achievement_model.dart';
 import 'package:focus_tracker/widgets/leading_icon.dart';
-import 'package:hive/hive.dart';
 
-class AchievementsScreen extends StatefulWidget {
+class AchievementsScreen extends StatelessWidget {
   const AchievementsScreen({super.key});
 
   @override
-  State<AchievementsScreen> createState() => _AchievementsScreenState();
-}
-
-class _AchievementsScreenState extends State<AchievementsScreen> {
-  final Box<AchievementModel> achievementsBox = Hive.box<AchievementModel>(
-    'achievementsBox',
-  );
-  @override
-  void initState() {
-    super.initState();
-    initializeAchievements();
-  }
-
-  void initializeAchievements() async {
-    if (achievementsBox.isEmpty) {
-      List<AchievementModel> achievements = [
-        AchievementModel(
-          title: "Beginner 🎯",
-          description: "Complete the first focus session",
-        ),
-        AchievementModel(
-          title: "Diligent 🔥",
-          description: "Complete 5 focus sessions",
-        ),
-        AchievementModel(
-          title: "Halfway 🏆",
-          description: "Achieve 50% of your weekly goal",
-        ),
-        AchievementModel(
-          title: "Champion 🚀",
-          description: "Achieve 100% of your weekly goal",
-        ),
-      ];
-
-      for (var achievement in achievements) {
-        achievementsBox.add(achievement);
-      }
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "🏅 Achievements",
-          style: TextStyle(fontWeight: FontWeight.w200),
-        ),
+    return BlocBuilder<StatsCubit, StatsCubitState>(
+      builder: (context, state) {
+        final cubit = context.read<StatsCubit>();
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "🏅 Achievements",
+              style: TextStyle(fontWeight: FontWeight.w200),
+            ),
 
-        leading: LeadingIcon(),
-      ),
-      body: ListView.builder(
-        itemCount: achievementsBox.length,
-        itemBuilder: (context, index) {
-          final achievement = achievementsBox.getAt(index) as AchievementModel;
+            leading: LeadingIcon(),
+          ),
+          body: ListView.builder(
+            itemCount: cubit.achievementsBox.length,
+            itemBuilder: (context, index) {
+              final achievement =
+                  cubit.achievementsBox.getAt(index) as AchievementModel;
 
-          return ListTile(
-            leading:
-                achievement.isUnlocked
-                    ? const Icon(
-                      Icons.emoji_events,
-                      color: Colors.amber,
-                      size: 32,
-                    )
-                    : const Icon(Icons.lock, color: Colors.grey, size: 32),
-            title: Text(achievement.title),
-            subtitle: Text(achievement.description),
-          );
-        },
-      ),
+              return ListTile(
+                leading:
+                    achievement.isUnlocked
+                        ? const Icon(
+                          Icons.emoji_events,
+                          color: Colors.amber,
+                          size: 32,
+                        )
+                        : const Icon(Icons.lock, color: Colors.grey, size: 32),
+                title: Text(achievement.title),
+                subtitle: Text(achievement.description),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:focus_tracker/main.dart';
-import 'package:focus_tracker/models/achievement_model/achievement_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:focus_tracker/cubit/stats_cubit/stats_cubit.dart';
 import 'package:focus_tracker/utils/components/text_field_border.dart';
 import 'package:focus_tracker/widgets/leading_icon.dart';
-import 'package:hive/hive.dart';
 import 'package:fl_chart/fl_chart.dart';
-import '../models/session_model/session_model.dart';
 
 class StatsScreen extends StatefulWidget {
   const StatsScreen({super.key});
@@ -18,248 +15,148 @@ class StatsScreen extends StatefulWidget {
 class _StatsScreenState extends State<StatsScreen> {
   final TextEditingController _goalController = TextEditingController();
 
-  List<FlSpot> _generateChartData(Box sessionBox) {
-    Map<int, int> weeklyData = {};
-
-    for (var i = 0; i < 7; i++) {
-      weeklyData[i] = 0;
-    }
-
-    for (var i = 0; i < sessionBox.length; i++) {
-      final session = sessionBox.getAt(i) as Session;
-      final int dayIndex = DateTime.now().difference(session.date).inDays;
-      if (dayIndex < 7) {
-        weeklyData[6 - dayIndex] =
-            (weeklyData[6 - dayIndex] ?? 0) + session.duration;
-      }
-    }
-
-    return weeklyData.entries
-        .map((entry) => FlSpot(entry.key.toDouble(), entry.value.toDouble()))
-        .toList();
-  }
-
-  List<String> generateWeekDays() {
-    List<String> weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    int todayIndex = DateTime.now().weekday; //
-    return [
-      ...weekDays.sublist(todayIndex),
-      ...weekDays.sublist(0, todayIndex),
-    ];
-  }
-
-  final Box goalBox = Hive.box('goalBox');
-  final Box sessionBox = Hive.box<Session>('sessionsBox');
-  final Box<AchievementModel> achievementsBox = Hive.box('achievementsBox');
-  void _saveGoal(int goal) async {
-    goalBox.put('weeklyGoal', goal);
-  }
-
-  int _getGoal() {
-    return goalBox.get('weeklyGoal', defaultValue: 300); // الافتراضي 300 دقيقة
-  }
-
-  late int goal = 300;
-  Future<void> _sendNotification(String title, String body) async {
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-          'productivity_channel',
-          'Productivity Notifications',
-          importance: Importance.high,
-          priority: Priority.high,
-        );
-
-    const NotificationDetails platformChannelSpecifics = NotificationDetails(
-      android: androidPlatformChannelSpecifics,
-    );
-
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      title,
-      body,
-      platformChannelSpecifics,
-    );
-  }
-
-  bool _hasSent50PercentNotification = false;
-  bool _hasSent100PercentNotification = false;
   @override
   Widget build(BuildContext context) {
-    int totalFocusTime = sessionBox.values.fold(
-      0,
-      (sum, session) => sum + (session as Session).duration,
-    );
-    int sessionCount = sessionBox.length;
-    int weeklyGoal = _getGoal();
-    double progress = totalFocusTime / weeklyGoal;
-    if (progress >= 0.5 && !_hasSent50PercentNotification) {
-      _sendNotification(
-        "🎯 Great Progress!",
-        "You've reached 50% of your weekly goal! 🚀",
-      );
-      _hasSent50PercentNotification = true;
-    }
-    void unlockAchievement(String title) {
-      for (var i = 0; i < achievementsBox.length; i++) {
-        final achievement = achievementsBox.getAt(i) as AchievementModel;
-        if (achievement.title == title && !achievement.isUnlocked) {
-          achievement.isUnlocked = true;
-          achievement.save();
-        }
-      }
-    }
+    return BlocBuilder<StatsCubit, StatsCubitState>(
+      builder: (context, state) {
+        final cubit = context.read<StatsCubit>();
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Productivity Stats',
+              style: TextStyle(fontWeight: FontWeight.w200),
+            ),
 
-    if (progress >= 1.0 && !_hasSent100PercentNotification) {
-      _sendNotification(
-        "🏆 Goal Achieved!",
-        "You've reached 100% of your weekly goal! 🎉",
-      );
-      _hasSent100PercentNotification = true;
-    }
-    if (sessionCount >= 1) {
-      unlockAchievement("Beginner 🎯");
-    }
-    if (sessionCount >= 5) {
-      unlockAchievement("Diligent 🔥");
-    }
-    if (progress >= 0.5) {
-      unlockAchievement("Halfway 🏆");
-    }
-
-    if (progress >= 1.0) {
-      unlockAchievement("Champion 🚀");
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Productivity Stats',
-          style: TextStyle(fontWeight: FontWeight.w200),
-        ),
-
-        leading: LeadingIcon(),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              setState(() {
-                goalBox.clear();
-                sessionBox.clear();
-                achievementsBox.clear();
-                _hasSent50PercentNotification = false;
-                _hasSent100PercentNotification = false;
-              });
-            },
+            leading: LeadingIcon(),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    cubit.goalBox.clear();
+                    cubit.sessionBox.clear();
+                    cubit.achievementsBox.clear();
+                    cubit.hasSent50PercentNotification = false;
+                    cubit.hasSent100PercentNotification = false;
+                  });
+                },
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(32),
-        child: ListView(
-          children: [
-            Row(
+          body: Padding(
+            padding: const EdgeInsets.all(32),
+            child: ListView(
               children: [
-                Text(
-                  'Total Focus Time: $totalFocusTime minutes',
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  'Completed Sessions: $sessionCount',
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: SizedBox(
-                height: 200,
-                child: LineChart(
-                  LineChartData(
-                    gridData: const FlGridData(show: false),
-                    borderData: FlBorderData(
-                      show: true,
-                      border: Border.all(color: Colors.grey, width: 1),
+                Row(
+                  children: [
+                    Text(
+                      'Total Focus Time: ${cubit.totalFocusTime()} minutes',
+                      style: const TextStyle(fontSize: 16),
                     ),
-                    titlesData: FlTitlesData(
-                      topTitles: const AxisTitles(
-                        sideTitles: SideTitles(showTitles: false),
-                      ),
-                      leftTitles: const AxisTitles(
-                        sideTitles: SideTitles(showTitles: false),
-                      ),
-                      bottomTitles: AxisTitles(
-                        sideTitles: SideTitles(
-                          showTitles: true,
-                          getTitlesWidget: (value, meta) {
-                            List<String> days = generateWeekDays();
-                            return Text(
-                              days[value.toInt()],
-                              style: const TextStyle(fontSize: 12),
-                            );
-                          },
-                        ),
-                      ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Completed Sessions: ${cubit.sessionCount()}',
+                      style: const TextStyle(fontSize: 16),
                     ),
-                    lineBarsData: [
-                      LineChartBarData(
-                        spots: _generateChartData(sessionBox),
-                        isCurved: true,
-                        color: Colors.blue,
-                        barWidth: 4,
-                        belowBarData: BarAreaData(
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: SizedBox(
+                    height: 200,
+                    child: LineChart(
+                      LineChartData(
+                        gridData: const FlGridData(show: false),
+                        borderData: FlBorderData(
                           show: true,
-                          color: Colors.blue.withAlpha(50),
+                          border: Border.all(color: Colors.grey, width: 1),
                         ),
+                        titlesData: FlTitlesData(
+                          topTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                          leftTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (value, meta) {
+                                List<String> days = cubit.generateWeekDays();
+                                return Text(
+                                  days[value.toInt()],
+                                  style: const TextStyle(fontSize: 12),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: cubit.generateChartData(cubit.sessionBox),
+                            isCurved: true,
+                            color: Colors.blue,
+                            barWidth: 4,
+                            belowBarData: BarAreaData(
+                              show: true,
+                              color: Colors.blue.withAlpha(50),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-            TextField(
-              controller: _goalController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                hintText: "Set weekly productivity goal (minutes)",
-                border: border(),
-                focusedBorder: border(),
-                enabledBorder: border(),
-                focusedErrorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: const BorderSide(color: Colors.red),
+                TextField(
+                  controller: _goalController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: "Set weekly productivity goal (minutes)",
+                    border: border(),
+                    focusedBorder: border(),
+                    enabledBorder: border(),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(color: Colors.red),
+                    ),
+                  ),
+                  onSubmitted: (value) {
+                    cubit.goal =
+                        int.tryParse(value) ??
+                        300; // إذا لم يدخل المستخدم قيمة صحيحة
+                    setState(() {
+                      cubit.saveGoal(cubit.goal);
+                    });
+                  },
                 ),
-              ),
-              onSubmitted: (value) {
-                goal =
-                    int.tryParse(value) ??
-                    300; // إذا لم يدخل المستخدم قيمة صحيحة
-                _saveGoal(goal);
-                setState(() {});
-              },
-            ),
-            const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-            LinearProgressIndicator(
-              borderRadius: BorderRadius.circular(8),
-              value: progress > 1 ? 1 : progress, // لا يتجاوز 100%
-              backgroundColor: Colors.grey[300],
-              valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-            ),
-            const SizedBox(height: 20),
-            Text('Weekly Goal Minutes: $goal'),
-            Text('Minutes Achieved: $totalFocusTime'),
+                LinearProgressIndicator(
+                  borderRadius: BorderRadius.circular(8),
+                  value:
+                      cubit.progress() > 1
+                          ? 1
+                          : cubit.progress(), // لا يتجاوز 100%
+                  backgroundColor: Colors.grey[300],
+                  valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+                const SizedBox(height: 20),
+                Text('Weekly Goal Minutes: ${cubit.goal}'),
+                Text('Minutes Achieved: ${cubit.totalFocusTime()}'),
 
-            Text(
-              "Weekly Goal Achieved: ${(progress * 100).toStringAsFixed(0)}%",
+                Text(
+                  "Weekly Goal Achieved: ${(cubit.progress() * 100).toStringAsFixed(0)}%",
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 
