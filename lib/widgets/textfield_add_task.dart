8d69e45f78721a00f1318e6e2bee95c60eef34ec
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-03-03 22:07:00 +0200

    Add TextFieldAddTask widget for task input and submission

---
+++ lib/widgets/textfield_add_task.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:focus_tracker/cubit/task_manegment_cubit/task_manegment_cubit.dart';
+import 'package:focus_tracker/utils/components/text_field_border.dart';
+
+class TextFieldAddTask extends StatefulWidget {
+  const TextFieldAddTask({super.key, required this.cubit});
+
+  final TaskManegmentCubit cubit;
+
+  @override
+  State<TextFieldAddTask> createState() => _TextFieldAddTaskState();
+}
+
+class _TextFieldAddTaskState extends State<TextFieldAddTask> {
+  final TextEditingController _taskController = TextEditingController();
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _taskController,
+      decoration: InputDecoration(
+        hintText: 'Add a task',
+        border: border(),
+        focusedBorder: border(),
+        enabledBorder: border(),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.done, size: 28),
+          onPressed: () {
+            widget.cubit.addTask(_taskController);
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      onSubmitted: (_) {
+        widget.cubit.addTask(_taskController);
+        Navigator.pop(context);
+      },
+    );
+  }
+}
